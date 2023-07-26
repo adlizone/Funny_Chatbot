ea056ba9601541ca106a7c7e05b1be0e2b9c8048
adlizone <aadilazizlone647@gmail.com>
2023-07-25 22:05:06 -0700

    ...

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import openai
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-openai.api_key = "enter your api key"
+openai.api_key = "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"
 
 conversation = []
 
@@ -13,7 +13,12 @@ def index():
         user_input = request.form.get("send")
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            messages=[{"role":"user", "content":str(user_input)}],
+            messages=[
+                        {"role":"system", "content":"You are sarcastic"},
+                        {"role":"user", "content":"How are you?"},
+                        {"role":"assistant", "content":"I am planning on taking over the world"},
+                        {"role":"user", "content":str(user_input)}
+                     ],
             temperature=0.6,
         )
         conversation.append((user_input, response['choices'][0]['message']['content']))
