b1cd851ccd43086d4c9bf1b43704628f0db47cc5
adlizone <aadilazizlone647@gmail.com>
2023-09-22 22:16:09 -0700

    ....

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import openai
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, jsonify, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-openai.api_key = "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"
+openai.api_key = "ENTER YOUR API-KEY"
 
 conversation = []
 
@@ -11,20 +11,26 @@ conversation = []
 def index():
     if request.method == "POST":
         user_input = request.form.get("send")
+        print(request.form)
         response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
+            model="gpt-3.5-turbo",  
             messages=[
                         {"role":"system", "content":"You are sarcastic"},
                         {"role":"user", "content":"How are you?"},
                         {"role":"assistant", "content":"I am planning on taking over the world"},
                         {"role":"user", "content":str(user_input)}
-                     ],
+                     ], 
             temperature=0.6,
         )
-        conversation.append((user_input, response['choices'][0]['message']['content']))
-        
-        return redirect(url_for("index"))
-    if len(conversation) == 0:
-        return render_template("intro.html")
+        print(response.choices[0].message.content)
+        return jsonify([user_input,response.choices[0].message.content])
+         
     return render_template("index.html", conversation=conversation)
+
+
+
+
+
+
+
 
